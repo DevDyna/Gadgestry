bd9c8e8b36cedbbca212d092c5a232947061371a
DevDyna <andrearisorti2003@gmail.com>
2024-08-07 21:30:28 +0200

    Update ToolItem.java

--- src/main/java/com/devdyna/gadgestry/common/register/item/ToolItem.java
+++ src/main/java/com/devdyna/gadgestry/common/register/item/ToolItem.java
@@ -1,14 +1,12 @@
 package com.devdyna.gadgestry.common.register.item;
 
-import com.devdyna.gadgestry.Gadgestry;
-
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.HoeItem;
 
-public class ToolItem {
-    public static final RegistryObject<Item> CROOK = Gadgestry.ITEMS.register("crook",
+public class ToolItem extends BasicItem{
+    public static final RegistryObject<Item> CROOK = ITEMS.register("crook",
             () -> new HoeItem(Tiers.IRON, 0, 0, new Item.Properties()));
 
 }
