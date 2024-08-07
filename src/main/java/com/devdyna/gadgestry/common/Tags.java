8517cac231de62346aabe576fe908f2392ce2dd7
DevDyna <andrearisorti2003@gmail.com>
2024-08-07 21:30:23 +0200

    Update Tags.java

--- src/main/java/com/devdyna/gadgestry/common/Tags.java
+++ src/main/java/com/devdyna/gadgestry/common/Tags.java
@@ -6,10 +6,9 @@ import net.minecraft.tags.TagKey;
 import com.devdyna.gadgestry.Gadgestry;
 import net.minecraft.world.level.block.Block;
 
-
 public class Tags {
     public static class Blocks {
-       public static final TagKey<Block> NEEDS_AQUAMARINE_TOOL = tag("needs_aquamarine_tool");
+        public static final TagKey<Block> NEEDS_AQUAMARINE_TOOL = tag("needs_aquamarine_tool");
 
         private static TagKey<Block> tag(String name) {
             return BlockTags.create(new ResourceLocation(Gadgestry.MODID, name));
