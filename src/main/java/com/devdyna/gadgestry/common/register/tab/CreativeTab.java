eeddc869f56215842041e6231cbee39dbb18fe5b
DevDyna <andrearisorti2003@gmail.com>
2024-08-08 01:22:43 +0200

    Update CreativeTab.java

--- src/main/java/com/devdyna/gadgestry/common/register/tab/CreativeTab.java
+++ src/main/java/com/devdyna/gadgestry/common/register/tab/CreativeTab.java
@@ -2,6 +2,7 @@ package com.devdyna.gadgestry.common.register.tab;
 
 import com.devdyna.gadgestry.Gadgestry;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
@@ -16,13 +17,27 @@ public class CreativeTab {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister
             .create(Registries.CREATIVE_MODE_TAB, Gadgestry.MODID);
 
-    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab",
+    public static final RegistryObject<CreativeModeTab> TAB = CREATIVE_MODE_TABS.register("tab",
             () -> CreativeModeTab.builder()
                     .withTabsBefore(CreativeModeTabs.COMBAT)
                     .icon(() -> new ItemStack(BasicItem.SCOOP.get()))
+                    .title(Component.translatable("tab.gadgestry.name"))
                     .displayItems((parameters, output) -> {
-                        output.accept(BasicItem.SCOOP.get()); // Add the example item to the tab. For your own tabs, this
-                                                           // method is preferred over the event
+                        output.accept(BasicItem.SCOOP.get());
+                        output.accept(BasicItem.FLOREAL_BONEMEAL.get());
+                        output.accept(BasicItem.MINERAL_BONEMEAL.get());
+                        output.accept(BasicItem.BEEWAX.get());
+                        output.accept(BasicItem.BEE.get());
+                        output.accept(BasicItem.IOLITE.get());
+                        output.accept(BasicItem.JADE.get());
+                        output.accept(BasicItem.TOPAZ.get());
+                        output.accept(BasicItem.AQUAMARINE.get());
+                        output.accept(BasicItem.ITEM_TREASURE_POT.get());
+                        output.accept(BasicItem.ITEM_FANCY_LABORATORY_COMBINER.get());
+                        output.accept(BasicItem.ITEM_FANCY_LABORATORY_DISSOLVER.get());
+                        output.accept(BasicItem.CROOK.get());
+                        output.accept(BasicItem.AQUAMARINE_PICKAXE.get());
+                        output.accept(BasicItem.STEW.get());
                     }).build());
 
     public static void register(IEventBus eventBus) {
