4488e81c4fec079a2fd3b3fdeafb18b24e8b4ece
DevDyna <andrearisorti2003@gmail.com>
2024-08-07 21:30:25 +0200

    Update CreativeTab.java

--- src/main/java/com/devdyna/gadgestry/common/register/tab/CreativeTab.java
+++ src/main/java/com/devdyna/gadgestry/common/register/tab/CreativeTab.java
@@ -1,22 +1,32 @@
 package com.devdyna.gadgestry.common.register.tab;
 
 import com.devdyna.gadgestry.Gadgestry;
-
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
-import com.devdyna.gadgestry.common.register.item.FoodItem;
+
+import com.devdyna.gadgestry.common.register.item.BasicItem;
 
 public class CreativeTab {
 
-    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = Gadgestry.CREATIVE_MODE_TABS.register(
-            Gadgestry.MODID + "_tab",
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister
+            .create(Registries.CREATIVE_MODE_TAB, Gadgestry.MODID);
+
+    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab",
             () -> CreativeModeTab.builder()
                     .withTabsBefore(CreativeModeTabs.COMBAT)
-                    .icon(() -> FoodItem.STEW.get().getDefaultInstance())
+                    .icon(() -> new ItemStack(BasicItem.SCOOP.get()))
                     .displayItems((parameters, output) -> {
-                        output.accept(FoodItem.STEW.get());
+                        output.accept(BasicItem.SCOOP.get()); // Add the example item to the tab. For your own tabs, this
+                                                           // method is preferred over the event
                     }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
 
 }
