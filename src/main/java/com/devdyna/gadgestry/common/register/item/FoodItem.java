293b321786dd6dec7067ea738d397c825fd10480
DevDyna <andrearisorti2003@gmail.com>
2024-08-07 21:30:32 +0200

    Update FoodItem.java

--- src/main/java/com/devdyna/gadgestry/common/register/item/FoodItem.java
+++ src/main/java/com/devdyna/gadgestry/common/register/item/FoodItem.java
@@ -1,13 +1,11 @@
 package com.devdyna.gadgestry.common.register.item;
 
-import com.devdyna.gadgestry.Gadgestry;
-
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.RegistryObject;
 
-public class FoodItem {
-    public static final RegistryObject<Item> STEW = Gadgestry.ITEMS.register("stew",
+public class FoodItem extends BasicItem{
+    public static final RegistryObject<Item> STEW = ITEMS.register("stew",
             () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
                     .alwaysEat().nutrition(3).saturationMod(5f).build())));
 }
