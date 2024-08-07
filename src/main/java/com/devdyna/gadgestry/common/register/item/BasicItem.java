186113f356754ff099b9fc81ae0fe006514bb96d
DevDyna <andrearisorti2003@gmail.com>
2024-08-07 21:30:34 +0200

    Update BasicItem.java

--- src/main/java/com/devdyna/gadgestry/common/register/item/BasicItem.java
+++ src/main/java/com/devdyna/gadgestry/common/register/item/BasicItem.java
@@ -3,42 +3,45 @@ package com.devdyna.gadgestry.common.register.item;
 import com.devdyna.gadgestry.Gadgestry;
 
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class BasicItem {
 
-    public static final RegistryObject<Item> SCOOP = Gadgestry.ITEMS.register("scoop",
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+            Gadgestry.MODID);
+
+    public static final RegistryObject<Item> SCOOP = ITEMS.register("scoop",
             () -> new Item(new Item.Properties().durability(128)));
 
-            public static final RegistryObject<Item> FLOREAL_BONEMEAL = Gadgestry.ITEMS.register("floreal bonemeal",
+    public static final RegistryObject<Item> FLOREAL_BONEMEAL = ITEMS.register("floreal_bonemeal",
             () -> new Item(new Item.Properties()));
 
-            public static final RegistryObject<Item> MINERAL_BONEMEAL = Gadgestry.ITEMS.register("mineral bonemeal",
+    public static final RegistryObject<Item> MINERAL_BONEMEAL = ITEMS.register("mineral_bonemeal",
             () -> new Item(new Item.Properties()));
 
-            public static final RegistryObject<Item> BEEWAX = Gadgestry.ITEMS.register("beewax",
+    public static final RegistryObject<Item> BEEWAX = ITEMS.register("beewax",
             () -> new Item(new Item.Properties()));
 
-            public static final RegistryObject<Item> BEE = Gadgestry.ITEMS.register("bee",
+    public static final RegistryObject<Item> BEE = ITEMS.register("bee",
             () -> new Item(new Item.Properties()));
 
-            public static final RegistryObject<Item> IOLITE = Gadgestry.ITEMS.register("iolite",
+    public static final RegistryObject<Item> IOLITE = ITEMS.register("iolite",
             () -> new Item(new Item.Properties()));
 
-            public static final RegistryObject<Item> JADE = Gadgestry.ITEMS.register("jade",
+    public static final RegistryObject<Item> JADE = ITEMS.register("jade",
             () -> new Item(new Item.Properties()));
 
-            public static final RegistryObject<Item> TOPAZ = Gadgestry.ITEMS.register("topaz",
+    public static final RegistryObject<Item> TOPAZ = ITEMS.register("topaz",
             () -> new Item(new Item.Properties()));
 
-            public static final RegistryObject<Item> AQUAMARINE = Gadgestry.ITEMS.register("aquamarine",
+    public static final RegistryObject<Item> AQUAMARINE = ITEMS.register("aquamarine",
             () -> new Item(new Item.Properties()));
 
-
-
-
-
-
-
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 
 }
