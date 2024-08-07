ffe52aeecbe309cd5a79560dba5553a59ad8ef11
DevDyna <andrearisorti2003@gmail.com>
2024-08-08 01:22:36 +0200

    Update BasicBlock.java

--- src/main/java/com/devdyna/gadgestry/common/register/block/BasicBlock.java
+++ src/main/java/com/devdyna/gadgestry/common/register/block/BasicBlock.java
@@ -1,6 +1,8 @@
 package com.devdyna.gadgestry.common.register.block;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,14 +17,18 @@ public class BasicBlock {
                         Gadgestry.MODID);
 
         public static final RegistryObject<Block> TREASURE_POT = BLOCKS.register("treasure_pot",
-                        () -> new Block(BlockBehaviour.Properties.of()));
+                        () -> new Pot(BlockBehaviour.Properties.copy(Blocks.DECORATED_POT)
+                                        .sound(SoundType.DECORATED_POT_CRACKED)));
 
-                        public static final RegistryObject<Block> FANCY_LABORATORY_BREAKER = BLOCKS.register("fancy_laboratory_breaker",
-                        () -> new Block(BlockBehaviour.Properties.of()));
+        public static final RegistryObject<Block> FANCY_LABORATORY_COMBINER = BLOCKS
+                        .register("fancy_laboratory_combiner",
+                                        () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)
+                                                        .sound(SoundType.METAL).noCollission()));
 
-                        public static final RegistryObject<Block> FANCY_LABORATORY_CRAFTER = BLOCKS.register("fancy_laboratory_crafter",
-                        () -> new Block(BlockBehaviour.Properties.of()));
-
+        public static final RegistryObject<Block> FANCY_LABORATORY_DISSOLVER = BLOCKS.register(
+                        "fancy_laboratory_dissolver",
+                        () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.METAL)
+                                        .noCollission()));
 
         public static void register(IEventBus eventBus) {
                 BLOCKS.register(eventBus);
