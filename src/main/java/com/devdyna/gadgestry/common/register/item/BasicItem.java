944a9d275b69b538b7122f146f065ec464b9a7d1
DevDyna <andrearisorti2003@gmail.com>
2024-08-29 01:54:12 +0200

    added all new gems and stuff related part 1

--- src/main/java/com/devdyna/gadgestry/common/register/item/BasicItem.java
+++ src/main/java/com/devdyna/gadgestry/common/register/item/BasicItem.java
@@ -3,12 +3,14 @@ package com.devdyna.gadgestry.common.register.item;
 import com.devdyna.gadgestry.Gadgestry;
 import com.devdyna.gadgestry.common.register.block.BasicBlock;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.HoeItem;
+//import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.PickaxeItem;
-import net.minecraft.world.item.Tiers;
+import net.minecraft.world.item.*;
+//import net.minecraft.world.item.Tiers;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,19 +21,97 @@ public class BasicItem {
         public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
                         Gadgestry.MODID);
 
+        // APIARY
+        // -------------------------------------------------------------------//
         public static final RegistryObject<Item> SCOOP = ITEMS.register("scoop",
                         () -> new Item(new Item.Properties().durability(128)));
-
-        public static final RegistryObject<Item> FLOREAL_BONEMEAL = ITEMS.register("floreal_bonemeal",
-                        () -> new Item(new Item.Properties()));
-
-        public static final RegistryObject<Item> MINERAL_BONEMEAL = ITEMS.register("mineral_bonemeal",
-                        () -> new Item(new Item.Properties()));
 
         public static final RegistryObject<Item> BEEWAX = ITEMS.register("beewax",
                         () -> new Item(new Item.Properties()));
 
         public static final RegistryObject<Item> BEE = ITEMS.register("bee",
+                        () -> new Item(new Item.Properties()));
+
+        // -------------------------------------------------------------------//
+
+        // GATHERING
+        // --------------------------------------------------------------------------------------//
+
+        public static final RegistryObject<Item> POTASSIUM = ITEMS.register("potassium",
+                        () -> new Item(new Item.Properties()));
+
+        public static final RegistryObject<Item> RESIN = ITEMS.register("resin",
+                        () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
+                                        .alwaysEat().nutrition(1).saturationMod(1f)
+                                        .effect(() -> new MobEffectInstance(MobEffects.WITHER, 1, 200), 75).build())));
+
+        public static final RegistryObject<Item> PATINA = ITEMS.register("patina",
+                        () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
+                                        .alwaysEat().nutrition(1).saturationMod(0f)
+                                        .effect(() -> new MobEffectInstance(MobEffects.POISON, 0, 1000), 75).build())));
+
+        public static final RegistryObject<Item> RAW_COPPER_NUGGET = ITEMS.register("raw_copper_nugget",
+                        () -> new Item(new Item.Properties()));
+
+        public static final RegistryObject<Item> RAW_GOLD_NUGGET = ITEMS.register("raw_gold_nugget",
+                        () -> new Item(new Item.Properties()));
+
+        public static final RegistryObject<Item> RAW_IRON_NUGGET = ITEMS.register("raw_iron_nugget",
+                        () -> new Item(new Item.Properties()));
+
+        // TOOLS
+        // -------------------------------------------------------------------//
+        // ----------//AQUAMARINE
+        public static final RegistryObject<Item> AQUAMARINE_PICKAXE = ITEMS.register("aquamarine_pickaxe",
+                        () -> new PickaxeItem(TierTool.AQUAMARINE, 2, 1, new Item.Properties()));
+
+        public static final RegistryObject<Item> AQUAMARINE_AXE = ITEMS.register("aquamarine_axe",
+                        () -> new AxeItem(TierTool.AQUAMARINE, 2, 1, new Item.Properties()));
+
+        public static final RegistryObject<Item> AQUAMARINE_SWORD = ITEMS.register("aquamarine_sword",
+                        () -> new SwordItem(TierTool.AQUAMARINE, 2, 1, new Item.Properties()));
+
+        public static final RegistryObject<Item> AQUAMARINE_HOE = ITEMS.register("aquamarine_hoe",
+                        () -> new HoeItem(TierTool.AQUAMARINE, 2, 1, new Item.Properties()));
+
+        public static final RegistryObject<Item> AQUAMARINE_SHOVEL = ITEMS.register("aquamarine_shovel",
+                        () -> new ShovelItem(TierTool.AQUAMARINE, 2, 1, new Item.Properties()));
+
+        // ----------//ROSE DIAMOND
+        public static final RegistryObject<Item> ROSE_DIAMOND_PICKAXE = ITEMS.register("rose_diamond_pickaxe",
+                        () -> new PickaxeItem(TierTool.ROSE_DIAMOND, 2, 1, new Item.Properties()));
+
+        public static final RegistryObject<Item> ROSE_DIAMOND_AXE = ITEMS.register("rose_diamond_axe",
+                        () -> new AxeItem(TierTool.ROSE_DIAMOND, 2, 1, new Item.Properties()));
+
+        public static final RegistryObject<Item> ROSE_DIAMOND_SWORD = ITEMS.register("rose_diamond_sword",
+                        () -> new SwordItem(TierTool.ROSE_DIAMOND, 2, 1, new Item.Properties()));
+
+        public static final RegistryObject<Item> ROSE_DIAMOND_HOE = ITEMS.register("rose_diamond_hoe",
+                        () -> new HoeItem(TierTool.ROSE_DIAMOND, 2, 1, new Item.Properties()));
+
+        public static final RegistryObject<Item> ROSE_DIAMOND_SHOVEL = ITEMS.register("rose_diamond_shovel",
+                        () -> new ShovelItem(TierTool.ROSE_DIAMOND, 2, 1, new Item.Properties()));
+        // -------------------------------------------------------------------//
+
+        // GEMS
+        // -------------------------------------------------------------------//
+        public static final RegistryObject<Item> APATITE = ITEMS.register("apatite",
+                        () -> new Item(new Item.Properties()));
+
+        public static final RegistryObject<Item> AQUAMARINE = ITEMS.register("aquamarine",
+                        () -> new Item(new Item.Properties()));
+
+        public static final RegistryObject<Item> BLUE_TANZANITE = ITEMS.register("blue_tanzanite",
+                        () -> new Item(new Item.Properties()));
+
+        public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
+                        () -> new Item(new Item.Properties()));
+
+        public static final RegistryObject<Item> DARK_QUARTZ = ITEMS.register("dark_quartz",
+                        () -> new Item(new Item.Properties()));
+
+        public static final RegistryObject<Item> GREEN_GARNET = ITEMS.register("green_garnet",
                         () -> new Item(new Item.Properties()));
 
         public static final RegistryObject<Item> IOLITE = ITEMS.register("iolite",
@@ -40,11 +120,27 @@ public class BasicItem {
         public static final RegistryObject<Item> JADE = ITEMS.register("jade",
                         () -> new Item(new Item.Properties()));
 
+        public static final RegistryObject<Item> KUNZITE = ITEMS.register("kunzite",
+                        () -> new Item(new Item.Properties()));
+
+        public static final RegistryObject<Item> OLIVINE = ITEMS.register("olivine",
+                        () -> new Item(new Item.Properties()));
+
+        public static final RegistryObject<Item> ROSE_DIAMOND = ITEMS.register("rose_diamond",
+                        () -> new Item(new Item.Properties()));
+
+        public static final RegistryObject<Item> SUNSTONE = ITEMS.register("sunstone",
+                        () -> new Item(new Item.Properties()));
+
         public static final RegistryObject<Item> TOPAZ = ITEMS.register("topaz",
                         () -> new Item(new Item.Properties()));
 
-        public static final RegistryObject<Item> AQUAMARINE = ITEMS.register("aquamarine",
+        public static final RegistryObject<Item> TSAVORITE = ITEMS.register("tsavorite",
                         () -> new Item(new Item.Properties()));
+        // --------------------------------------------------------------------------------------//
+
+        // BLOCK-ITEMS
+        // -------------------------------------------------------------------//
 
         public static final RegistryObject<Item> ITEM_TREASURE_POT = ITEMS.register("treasure_pot",
                         () -> new BlockItem(BasicBlock.TREASURE_POT.get(), new Item.Properties()));
@@ -57,19 +153,40 @@ public class BasicItem {
                         "fancy_laboratory_dissolver",
                         () -> new BlockItem(BasicBlock.FANCY_LABORATORY_DISSOLVER.get(), new Item.Properties()));
 
-        public static final RegistryObject<Item> CROOK = ITEMS.register("crook",
-                        () -> new HoeItem(Tiers.IRON, 1, 1, new Item.Properties()));
-
-        public static final RegistryObject<Item> AQUAMARINE_PICKAXE = ITEMS.register("aquamarine_pickaxe",
-                        () -> new PickaxeItem(TierTool.AQUAMARINE, 2, 1, new Item.Properties()));
-
-        public static final RegistryObject<Item> STEW = ITEMS.register("stew",
-                        () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
-                                        .alwaysEat().nutrition(3).saturationMod(5f).build())));
-
         public static final RegistryObject<Item> ITEM_ARCHEOLOGY_TABLE = ITEMS.register(
                         "archeology_table",
                         () -> new BlockItem(BasicBlock.ARCHEOLOGY_TABLE.get(), new Item.Properties()));
+
+        // -------------------------------------------------------------------//
+
+        // TO DO - NOT YET COMPLETLY IMPLEMENTED
+        // -------------------------------------------------------------------//
+        // public static final RegistryObject<Item> FLOREAL_BONEMEAL =
+        // ITEMS.register("floreal_bonemeal",
+        // () -> new Item(new Item.Properties()));
+
+        // public static final RegistryObject<Item> MINERAL_BONEMEAL =
+        // ITEMS.register("mineral_bonemeal",
+        // () -> new Item(new Item.Properties()));
+
+        // public static final RegistryObject<Item> CROOK = ITEMS.register("crook",
+        // () -> new HoeItem(Tiers.IRON, 1, 1, new Item.Properties()));
+        // public static final RegistryObject<Item> PAXEL = ITEMS.register("paxel",
+        // () -> new PickaxeItem(Tiers.IRON, 1, 1, new Item.Properties()));
+
+        // public static final RegistryObject<Item> STEW = ITEMS.register("stew",
+        // () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
+        // .alwaysEat().nutrition(3).saturationMod(5f).build())));
+
+        // -------------------------------------------------------------------//
+
+        public static final RegistryObject<Item> ROTTEN_BALL = ITEMS.register("rotten_ball",
+                        () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
+                                        .alwaysEat().nutrition(2).saturationMod(2f)
+                                        .effect(() -> new MobEffectInstance(MobEffects.HUNGER, 1, 200), 100).build())));
+
+        public static final RegistryObject<Item> GEM_TEMPLATE = ITEMS.register("gem_template",
+                        () -> new Item(new Item.Properties()));
 
         public static void register(IEventBus eventBus) {
                 ITEMS.register(eventBus);
--- src/main/java/com/devdyna/gadgestry/common/register/tab/CreativeTab.java
+++ src/main/java/com/devdyna/gadgestry/common/register/tab/CreativeTab.java
@@ -5,6 +5,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -23,21 +24,62 @@ public class CreativeTab {
                     .icon(() -> new ItemStack(BasicItem.SCOOP.get()))
                     .title(Component.translatable("tab.gadgestry.name"))
                     .displayItems((parameters, output) -> {
+                        // APIARY
+                        // --------------------------------------------------------------//
                         output.accept(BasicItem.SCOOP.get());
-                        output.accept(BasicItem.FLOREAL_BONEMEAL.get());
-                        output.accept(BasicItem.MINERAL_BONEMEAL.get());
                         output.accept(BasicItem.BEEWAX.get());
                         output.accept(BasicItem.BEE.get());
+                        // --------------------------------------------------------------//
+                        // GATHERING
+                        // -------------------------------------------------------------------//
+                        output.accept(BasicItem.POTASSIUM.get());
+                        output.accept(BasicItem.RESIN.get());
+                        output.accept(BasicItem.PATINA.get());
+                        output.accept(BasicItem.RAW_COPPER_NUGGET.get());
+                        output.accept(BasicItem.RAW_GOLD_NUGGET.get());
+                        output.accept(BasicItem.RAW_IRON_NUGGET.get());
+
+                        // TOOLS
+                        // --------------------------------------------------------------//
+                        //----------//AQUAMARINE
+                        output.accept(BasicItem.AQUAMARINE_AXE.get());
+                        output.accept(BasicItem.AQUAMARINE_HOE.get());
+                        output.accept(BasicItem.AQUAMARINE_PICKAXE.get());
+                        output.accept(BasicItem.AQUAMARINE_SWORD.get());
+                        output.accept(BasicItem.AQUAMARINE_SHOVEL.get());
+                        //----------//ROSE DIAMOND
+                        output.accept(BasicItem.ROSE_DIAMOND_AXE.get());
+                        output.accept(BasicItem.ROSE_DIAMOND_HOE.get());
+                        output.accept(BasicItem.ROSE_DIAMOND_PICKAXE.get());
+                        output.accept(BasicItem.ROSE_DIAMOND_SWORD.get());
+                        output.accept(BasicItem.ROSE_DIAMOND_SHOVEL.get());
+                        // GEMS
+                        // --------------------------------------------------------------//
+                        output.accept(BasicItem.APATITE.get());
+                        output.accept(BasicItem.AQUAMARINE.get());
+                        output.accept(BasicItem.BLUE_TANZANITE.get());
+                        output.accept(BasicItem.CITRINE.get());
+                        output.accept(BasicItem.DARK_QUARTZ.get());
+                        output.accept(BasicItem.GREEN_GARNET.get());
                         output.accept(BasicItem.IOLITE.get());
                         output.accept(BasicItem.JADE.get());
+                        output.accept(BasicItem.KUNZITE.get());
+                        output.accept(BasicItem.OLIVINE.get());
+                        output.accept(BasicItem.ROSE_DIAMOND.get());
+                        output.accept(BasicItem.SUNSTONE.get());
                         output.accept(BasicItem.TOPAZ.get());
-                        output.accept(BasicItem.AQUAMARINE.get());
+                        output.accept(BasicItem.TSAVORITE.get());
+                        // -------------------------------------------------------------------//
+                        // BLOCKITEM
+                        // --------------------------------------------------------------//
                         output.accept(BasicItem.ITEM_TREASURE_POT.get());
                         output.accept(BasicItem.ITEM_FANCY_LABORATORY_COMBINER.get());
                         output.accept(BasicItem.ITEM_FANCY_LABORATORY_DISSOLVER.get());
-                        output.accept(BasicItem.CROOK.get());
-                        output.accept(BasicItem.AQUAMARINE_PICKAXE.get());
-                        output.accept(BasicItem.STEW.get());
+                        output.accept(BasicItem.ITEM_ARCHEOLOGY_TABLE.get());
+                        // --------------------------------------------------------------//
+                        output.accept(BasicItem.ROTTEN_BALL.get());
+                        output.accept(BasicItem.GEM_TEMPLATE.get());
+
                     }).build());
 
     public static void register(IEventBus eventBus) {
